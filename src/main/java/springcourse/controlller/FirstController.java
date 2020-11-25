94b5ed3c590223a000d681623861ec8e0a1eebaf
18741847 <AOKulakov@sberbank.ru>
2020-11-25 17:32:51 +0300

    Done

--- src/main/java/springcourse/controlller/FirstController.java
+++ src/main/java/springcourse/controlller/FirstController.java
@@ -2,8 +2,10 @@ package springcourse.controlller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+//@RequestMapping("/first")  // стартовый URL для всех методов (можно удалить)
 public class FirstController {
 
     // возвращаем представление
