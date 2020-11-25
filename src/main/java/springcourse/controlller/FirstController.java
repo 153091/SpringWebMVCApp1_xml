3a17471d7445b8e5ae1843791aff5010d684ae86
18741847 <AOKulakov@sberbank.ru>
2020-11-25 17:15:04 +0300

    Done

--- src/main/java/springcourse/controlller/FirstController.java
+++ src/main/java/springcourse/controlller/FirstController.java
@@ -7,14 +7,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class FirstController {
 
     // возвращаем представление
-    @GetMapping("hello")
+    @GetMapping("/hello")
     public String helloPage() {
         return "first/hello";
     }
 
     // возвращаем представление
     @GetMapping("/goodbye")
-    public String goodByePage() {
+    public String goodbyePage() {
         return "first/goodbye";
     }
 }
