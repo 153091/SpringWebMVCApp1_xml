ef91405faf085abe33a8b83a52c1c064e14dce2b
18741847 <AOKulakov@sberbank.ru>
2020-11-26 13:08:13 +0300

    +calculator

--- src/main/java/springcourse/controlller/FirstController.java
+++ src/main/java/springcourse/controlller/FirstController.java
@@ -1,6 +1,7 @@
 package springcourse.controlller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,17 +12,23 @@ import javax.servlet.http.HttpServletRequest;
 //@RequestMapping("/first")  // стартовый URL для всех методов (можно удалить)
 public class FirstController {
 
-    // метод считывает параметры GET-запроса
-    // http://localhost:8080/hello?name=Ivan&surname=Ivanov
-    // required = false  -  параметры в запросе НЕ ОБЯЗАТЕЛЬНЫ
-    // если в запросе не указать параметры, то напечатает Hello, null null
-    // возвращаем представление
+    /** метод считывает параметры GET-запроса
+     http://localhost:8080/hello?name=Ivan&surname=Ivanov
+     required = false  -  параметры в запросе НЕ ОБЯЗАТЕЛЬНЫ
+     если в запросе не указать параметры, то напечатает Hello, null null
+
+     Model - передадим параметры Http GET-запроса в представление и отобразим их пользователю
+     возвращаем представление*/
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
 
         // если в запросе не указать параметры, то напечатает Hello, null null
         System.out.println("Hello, " + name + " " + surname);
+
+        // передадим параметры в Model
+        model.addAttribute("message", "Hello, " + name + " " + surname);
 
         return "first/hello";
     }
@@ -42,5 +49,35 @@ public class FirstController {
     @GetMapping("/goodbye")
     public String goodbyePage() {
         return "first/goodbye";
+    }
+
+    // http://localhost:8080/calculator?a=20&b=30&action=addition
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam("a") int a, @RequestParam("b") int b,
+                             @RequestParam("action") String action, Model model) {
+
+        double result;
+
+        switch (action) {
+            case "multiplication":
+                result = a * b;
+                break;
+            case "division":
+                result = a / (double) b;
+                break;
+            case "subtraction":
+                result = a - b;
+                break;
+            case "addition":
+                result = a + b;
+                break;
+            default:
+                result = 0;
+                break;
+        }
+
+        model.addAttribute("result", result);
+
+        return "first/calculator";
     }
 }
